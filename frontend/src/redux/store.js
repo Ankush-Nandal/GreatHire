cd37346db674f90fd3bda58d2243772aea1391ab
Virender Singh <virendersinghdev1@gmail.com>
2024-12-10 17:21:07 +0530

    adding redux-persit

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,46 +1,45 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // Defaults to localStorage for web
+
+// Import your slices
 import authSlice from "./authSlice";
-//import jobSlice from "./jobSlice";
-// import {
-//     persistStore,
-//     persistReducer,
-//     FLUSH,
-//     REHYDRATE,
-//     PAUSE,
-//     PERSIST,
-//     PURGE,
-//     REGISTER,
-// } from 'redux-persist'
-// import storage from 'redux-persist/lib/storage'
+// Uncomment these when the slices are ready
+// import jobSlice from "./jobSlice";
 // import companySlice from "./companySlice";
 // import applicationSlice from "./applicationSlice";
 
-// const persistConfig = {
-//     key: 'root',
-//     version: 1,
-//     storage,
-// }
+// Configure persist settings
+const persistConfig = {
+    key: "root", // Key for the persisted data
+    version: 1,
+    storage, // Define the storage engine (localStorage in this case)
+};
 
-// const rootReducer = combineReducers({
-//     auth:authSlice,
-//     job:jobSlice,
-//     company:companySlice,
-//     application:applicationSlice
-// })
-
-// const persistedReducer = persistReducer(persistConfig, rootReducer)
-
-
-const store = configureStore({
-    reducer:{
-        auth: authSlice
-    }
-    // reducer: persistedReducer,
-    // middleware: (getDefaultMiddleware) =>
-    //     getDefaultMiddleware({
-    //         serializableCheck: {
-    //             ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    //         },
-    //     }),
+// Combine reducers
+const rootReducer = combineReducers({
+    auth: authSlice,
+    // Uncomment these when the slices are ready
+    // job: jobSlice,
+    // company: companySlice,
+    // application: applicationSlice,
 });
+
+// Wrap the root reducer with persistReducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Configure the store
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+});
+
+// Persistor to persist the store
+export const persistor = persistStore(store);
+
 export default store;
